89c043baaa0803a2bc7089ca0f8aecd6dcd29114
Donald Oakes <donald.oakes@centurylink.com>
2019-02-22 15:02:44 -0700

    initial

--- assets/com/centurylink/mdw/demo/cool/CoolActivity.java
+++ assets/com/centurylink/mdw/demo/cool/CoolActivity.java
@@ -5,7 +5,6 @@ import com.centurylink.mdw.model.workflow.ActivityRuntimeContext;
 import com.centurylink.mdw.util.log.StandardLogger.LogLevel;
 import com.centurylink.mdw.util.timer.Tracked;
 import com.centurylink.mdw.workflow.activity.DefaultActivityImpl;
-import com.centurylink.mdwdemo.DemoHelper;
 
 @Tracked(LogLevel.TRACE)
 public class CoolActivity extends DefaultActivityImpl {
@@ -14,12 +13,9 @@ public class CoolActivity extends DefaultActivityImpl {
     public Object execute(ActivityRuntimeContext runtimeContext) throws ActivityException {
         runtimeContext.logInfo("Degree of coolness: " + runtimeContext.getAttribute("coolLevel"));
         runtimeContext.logInfo("Accessing helper...");
-        DemoHelper helper = new DemoHelper();
-        String something = helper.doSomething();
-        setVariableValue("something", something);
-        runtimeContext.logInfo("helper.doSomething(): " + something);
-        String greeting = helper.hello((String)getVariableValue("name"));
-        setVariableValue("greeting", greeting);
+        setVariableValue("something", "something");
+        runtimeContext.logInfo("helper.doSomething(): " + "something");
+        setVariableValue("greeting", "greeting");
         
         runtimeContext.logInfo("Updating input doc...");
         CoolDoc doc = (CoolDoc) runtimeContext.getVariables().get("doc");
